99755731627ea2f15058b2d8977909fe7318681e
KedaiVPN <caracarayt0@gmail.com>
2026-01-09 19:39:52 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -163,7 +163,7 @@ echo -e "$white\033[0;34m┌─────────────────�
 echo -e " \E[41;1;39m          ⇱ Install SSH / WS ⇲           \E[0m$NC"
 echo -e "$white\033[0;34m└─────────────────────────────────────────┘${NC}"
 sleep 1
-wget -q https://raw.githubusercontent.com/KedaiVPN/dynamic/ssh-vpn.sh && chmod +x ssh-vpn.sh && ./ssh-vpn.sh
+wget -q https://raw.githubusercontent.com/KedaiVPN/dynamic/main/ssh-vpn.sh && chmod +x ssh-vpn.sh && ./ssh-vpn.sh
 #install ins-xray
 echo -e "$white\033[0;34m┌─────────────────────────────────────────┐${NC}"
 echo -e " \E[41;1;39m            ⇱ Install Xray ⇲             \E[0m$NC"
