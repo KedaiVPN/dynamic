23fb1e7c1f97b95140c676156596d6595d95e10a
KedaiVPN <caracarayt0@gmail.com>
2026-02-28 09:23:29 +0700

    Update menu-vless.sh

--- menu-vless.sh
+++ menu-vless.sh
@@ -381,7 +381,7 @@ function trialvless(){
 
     vlesslink1="vless://${uuid}@${domain}:443?type=ws&encryption=none&security=tls&host=${domain}&path=/vless&allowInsecure=1&sni=${domain}#XRAY_VLESS_TLS_${user}"
     vlesslink2="vless://${uuid}@${domain}:80?type=ws&encryption=none&security=none&host=${domain}&path=/vless#XRAY_VLESS_NTLS_${user}"
-    vlesslink3="vless://${uuid}@${domain}:443?security=tls&encryption=none&type=grpc&serviceName=vless-grpc&sni=${domain}#VLESS_GRPC_${user}"
+    vlesslink3="vless://${uuid}@${domain}:443?mode=gun&security=tls&encryption=none&type=grpc&serviceName=vless-grpc&path=vless-grpc&sni=${domain}#VLESS_GRPC_${user}"
 
     # Restart Xray
     systemctl restart xray > /dev/null 2>&1
