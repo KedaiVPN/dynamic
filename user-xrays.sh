dafc3ccea2d967f0bcd7e58ba266af0be6b92521
KedaiVPN <caracarayt0@gmail.com>
2026-02-28 08:36:24 +0700

    Update user-xrays.sh

--- user-xrays.sh
+++ user-xrays.sh
@@ -44,7 +44,7 @@ hariini=`date -d "0 days" +"%Y-%m-%d"`
 
 vlesslink1="vless://${uuid}@${domain}:$tls?path=/vless&security=tls&encryption=none&type=ws#${user}"
 vlesslink2="vless://${uuid}@${domain}:$none?path=/vless&encryption=none&type=ws#${user}"
-vlesslink3="vless://${uuid}@${domain}:$tls?mode=gun&security=tls&encryption=none&type=grpc&serviceName=vless-grpc&sni=bug.com#${user}"
+vlesslink3="vless://${uuid}@${domain}:$tls?security=tls&encryption=none&type=grpc&serviceName=vless-grpc&sni=${domain}#${user}"
 
 clear && printf '\033[3J'
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
